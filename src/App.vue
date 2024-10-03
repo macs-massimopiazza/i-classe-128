cc51a1159bb027329ccea559a9b76f412a4e0514
ElonaHoxhaj96 <elona6915@gmail.com>
2024-10-03 16:50:05 +0200

    feat: new new text

--- src/App.vue
+++ src/App.vue
@@ -55,6 +55,7 @@ export default {
         },
         { src: null, testo: "Nananananana Batman 2", nome: "Simone" },
         { src: "", testo: "", nome: "" },
+        { src: "https://www.socialpilot.co/wp-content/uploads/2023/02/MEME.webp", testo: "essere o non essere, questo è il dilemma", nome: "Elona" },
       ],
     };
   },
